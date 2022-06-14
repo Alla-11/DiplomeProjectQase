5af55e74092ff27ad2aa9f0c72e1fd963134a529
Alla Z <aszhulyanova@gmail.com>
2022-06-15 00:43:54 +0300

    hid passwords

--- src/test/java/org/tms/models/UI/Users.java
+++ src/test/java/org/tms/models/UI/Users.java
@@ -2,9 +2,13 @@ package org.tms.models.UI;
 
 public class Users {
 
-    private String validName = "j768th@mail2run.com";
-    private String validPassword = "159zzz159";
-    private String invalidPassword = "111zzz159";
+    private String validName = System.getProperty("validusername");
+    private String validPassword = System.getProperty("validpassword");
+    private String invalidPassword= System.getProperty("invalidpassword");
+
+    //private String validName = "j768th@mail2run.com";
+    //private String validPassword = "159zzz159";
+    //private String invalidPassword = "111zzz159";
 
 
     public Users(){
