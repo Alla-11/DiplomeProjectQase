1623127b34cc35a84c071bb2e2f6f5a58e4947a3
Alla Z <aszhulyanova@gmail.com>
2022-06-15 01:14:12 +0300

    add Jenkinsfile

--- src/test/java/org/tms/adapters/BaseAdapter.java
+++ src/test/java/org/tms/adapters/BaseAdapter.java
@@ -9,10 +9,10 @@ import static org.tms.utils.StringConstant.TOKEN_NAME;
 public class BaseAdapter {
 
     private static final String BASE_URL = "https://api.qase.io/v1";
-    private static final String TOKEN_VALUE= System.getProperty("token");
+    //private static final String TOKEN_VALUE= System.getProperty("token");
 
 
-    //private static final String TOKEN_VALUE = "d1d4ed11a18254455b2d3433f4a6b74f78127f73";
+    private static final String TOKEN_VALUE = "d1d4ed11a18254455b2d3433f4a6b74f78127f73";
     Gson converter = new Gson();
 
     protected Response post(String url, String body){
